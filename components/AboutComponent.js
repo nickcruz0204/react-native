916cd52d5efafc3392ee205c0ad96e34b9a0c4c1
LordFagan <the.fagan@gmail.com>
2020-10-23 21:39:43 -0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,60 +1,56 @@
-import React, { Component} from "react";
-import  {ScrollView, Text, FlatList} from "react-native";
-import  {Card, ListItem} from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
+import React, { Component } from "react";
+import  {ScrollView, Text, FlatList } from "react-native";
+import  {Card, ListItem } from "react-native-elements";
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
-function Mission(){
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
+
+function Mission() {
     return (
-        <Card title={"Our Mission"} > 
-            <Text>
-            We present a curated database of the best campsites in the vast woods 
-            and backcountry of the World Wide Web Wilderness.
-            We increase access to adventure for the public while
-            promoting safe and respectful use of resources.
-            The expert wilderness trekkers on our staff personally verify each 
-            campsite to make sure that they are up to our standards. 
-            We also present a platform for campers to share reviews on campsites they have visited with each other.
+        <Card title="Our Mission"> 
+            <Text style ={{margin: 10}}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
-    )
+    );
 }
 
 class About extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        }
-    }
 
     static navigationOptions = {
-        title: "About US"
-    }
+        title: "About Us"
+    };
 
-    render(){
-        const renderItem = ({item}) => {
+    render() {
+        const renderPartner = ({item}) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require("./images/bootstrap-logo.png")}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
 
         return (
             <ScrollView>
-                <Mission/>
-                <Card title={"Community Partners"} >
+                <Mission />
+                <Card
+                    title="Community Partners">
                     <FlatList 
-                        data={this.state.partners}
-                        renderItem={renderItem}
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
-                        />
+                    />
                 </Card>
             </ScrollView>
         );
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
